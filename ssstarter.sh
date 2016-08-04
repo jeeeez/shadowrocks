29acd268749f700bb549b8b3cc7ae52088f18d13
picker <450994392@qq.com>
2016-08-04 09:26:37 +0800

    shadowsocks启动脚本

--- ssstarter.sh
+++ ssstarter.sh
@@ -4,12 +4,12 @@
 case $1 in
     start)
         echo -n "Starting shadowsocks"
-        nohup /usr/local/bin/ssserver -c /etc/shadowsocks.json > /usr/local/shadowsocks/log &
+        nohup /usr/bin/ssserver -c /etc/shadowsocks.json > /usr/local/shadowsocks/log &
         echo " done"
     ;;
     stop)
         echo -n "Stopping shadowsocks"
-        kill -9 `ps -ef|grep '/usr/bin/python /usr/local/bin/ssserver'|grep -v 'grep'|awk {'print $2'}`
+        kill -9 `ps -ef|grep '/usr/bin/python /usr/bin/ssserver'|grep -v 'grep'|awk {'print $2'}`
         echo " done"
     ;;
     restart)
