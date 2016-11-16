43f4f798423240bb98b25251487b08e12cf5cf4d
Picker Lee <450994392@qq.com>
2016-11-16 22:58:55 +0800

    添加初始化脚本

---
+++ initialize.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var exec = require('child_process').exec;
+
+var shadowsocksConfURL = '/etc/shadowsocks.json';
+
+var shadowsocksConf = require(shadowsocksConfURL);
+
+var portPasswords = shadowsocksConf.port_password;
+
+var arguments = process.argv.splice(2);
+
+shadowsocksConf.port_password = JSON.parse(arguments[0]);
+
+fs.writeFile(shadowsocksConfURL, JSON.stringify(shadowsocksConf, null, 4), function(error) {
+	if (error) {
+		return console.log(error);
+	}
+
+	console.log('配置写入成功！');
+	console.log('正在重新载入shadowrocks配置文件...');
+	exec(`${__dirname}/ssstarter.sh restart`, function(error, stdout, stderr) {
+		if (error) {
+			return console.log(error);
+		}
+
+		console.log(stdout);
+		console.log('重启成功!');
+	});
+});
